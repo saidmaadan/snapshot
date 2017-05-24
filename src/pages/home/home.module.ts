fdb0a4254baa80cf6c0866a0668353659bdc7b81
Said Maadan <me@saidmaadan.com>
2017-05-24 16:54:15 -0500

    Added pictures slideshow components

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { PipesModule} from '../../pipes/pipes.module';
 import { HomePage } from './home';
 
 @NgModule({
@@ -8,6 +9,7 @@ import { HomePage } from './home';
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
+    PipesModule
   ],
   exports: [
     HomePage
